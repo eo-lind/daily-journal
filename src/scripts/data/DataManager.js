77cbee301cf9646c333bfe528402659586021f7f
eo-lind <eo.lind@gmail.com>
2022-03-28 17:55:20 -0500

    added functional delete  button to posts

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,20 +1,27 @@
 export const getPosts = () => {
+  return fetch("http://localhost:8088/posts?_sort=id&_order=desc")
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      return parsedResponse;
+    });
+};
 
-    return fetch("http://localhost:8088/posts?_sort=id&_order=desc")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        return parsedResponse;
-    })
-}
+export const createPost = (postObj) => {
+  return fetch("http://localhost:8088/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postObj),
+  }).then((response) => response.json());
+};
 
-export const createPost = postObj => {
-    return fetch("http://localhost:8088/posts", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(postObj)
-  
-    })
-        .then(response => response.json())
-  }
+// to fetch a specific post from the database to be deleted
+export const deletePost = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,83 +1,94 @@
-import { getPosts, createPost } from "./data/DataManager.js";
+import { getPosts, createPost, deletePost } from "./data/DataManager.js";
 import { PostList } from "./feed/PostList.js";
 import { PostEntry } from "./feed/PostEntry.js";
 
 const showPostList = () => {
-    //Get a reference to the location on the DOM where the list will display
-    const postElement = document.querySelector(".postList");
-    getPosts().then((allPosts) => {
-        postElement.innerHTML = PostList(allPosts);
-    })
-}
+  //Get a reference to the location on the DOM where the list will display
+  const postElement = document.querySelector(".postList");
+  getPosts().then((allPosts) => {
+    postElement.innerHTML = PostList(allPosts);
+  });
+};
 
-  // to display the post submission form
-  const showPostEntry = () => { 
-    //Get a reference to the location on the DOM where the nav will display
-    const entryElement = document.querySelector(".entryForm");
-    entryElement.innerHTML = PostEntry();
-  }
+// to display the post submission form
+const showPostEntry = () => {
+  //Get a reference to the location on the DOM where the nav will display
+  const entryElement = document.querySelector(".entryForm");
+  entryElement.innerHTML = PostEntry();
+};
 
 /**
  * Main logic module for what should happen on initial page load for the journal
  */
 
 const startJournal = () => {
-    const postElement = document.querySelector(".postList");
-	// postElement.innerHTML = ""
-    showPostList()
-    showPostEntry()
-}
+  const postElement = document.querySelector(".postList");
+  // postElement.innerHTML = ""
+  showPostList();
+  showPostEntry();
+};
 
 startJournal();
-
 
 // event listeners
 
 // listening for clicks anywhere in <main>
-const applicationElement = document.querySelector("main")
+const applicationElement = document.querySelector("main");
+
+// clicks on thhe delete button
+
+applicationElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.id.startsWith("delete")) {
+    const postId = event.target.id.split("--")[1];
+    deletePost(postId).then((response) => {
+      showPostList();
+    });
+  }
+});
 
 // clicks on the edit button
 applicationElement.addEventListener("click", (event) => {
-	
-	if (event.target.id.startsWith("edit")){
-		console.log("post clicked", event.target.id.split("--"))
-		console.log("the id is", event.target.id.split("--")[1])
-	}
-})
+  if (event.target.id.startsWith("edit")) {
+    console.log("post clicked", event.target.id.split("--"));
+    console.log("the id is", event.target.id.split("--")[1]);
+  }
+});
 
 // clicks on the cancel new post button
-applicationElement.addEventListener("click", event => {
-    if (event.target.id === "newPost__cancel") {
-        // clear the input fields
-        document.getElementById('conceptsCovered').value = ''
-        document.getElementById('mood').value = ''
-        document.getElementById('journalEntry').value = ''
-    }
-  })
-  
-  // clicks on the submit new post button
-  applicationElement.addEventListener("click", event => {
-    event.preventDefault();
-    if (event.target.id === "newPost__submit") {
+applicationElement.addEventListener("click", (event) => {
+  if (event.target.id === "newPost__cancel") {
+    // clear the input fields
+    document.getElementById("conceptsCovered").value = "";
+    document.getElementById("mood").value = "";
+    document.getElementById("journalEntry").value = "";
+  }
+});
+
+// clicks on the submit new post button
+applicationElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.id === "newPost__submit") {
     // collect the input values into an object to post to the DB
-      const concept = document.querySelector("input[name='postTitle']").value
-      const mood = document.querySelector("input[name='postMood']").value
-      const entry = document.querySelector("textarea[name='postDescription']").value
-      //we have not created a user yet - for now, we will hard code `1`.
-      //we can add the current time as well
-      const postObject = {
-          timestamp: Date.now(),
-          concept: concept,
-          entry: entry,
-          mood: mood
-      }
-  
-        createPost(postObject)
-        .then(()=>{
-			showPostList()
-			document.getElementById('conceptsCovered').value = ''
-            document.getElementById('mood').value = ''
-            document.getElementById('journalEntry').value = ''
-		})
-    }
-  })
+    const concept = document.querySelector("input[name='postTitle']").value;
+    const mood = document.querySelector("input[name='postMood']").value;
+    const entry = document.querySelector(
+      "textarea[name='postDescription']"
+    ).value;
+    //we have not created a user yet - for now, we will hard code `1`.
+    //we can add the current time as well
+    const postObject = {
+      timestamp: Date.now(),
+      concept: concept,
+      entry: entry,
+      mood: mood,
+    };
+
+    createPost(postObject).then(() => {
+      showPostList();
+      document.getElementById("conceptsCovered").value = "";
+      document.getElementById("mood").value = "";
+      document.getElementById("journalEntry").value = "";
+    });
+  }
+});
